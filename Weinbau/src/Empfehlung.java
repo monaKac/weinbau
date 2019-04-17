ed72677a7b819e6962cce7491175b68967129f59
D074003 <D074003@WDFN34367190A.emea.global.corp.sap>
2019-04-17 13:03:08 +0200

    Hinzufügen der empfehlungBewaesserung() Logik

--- Weinbau/src/Empfehlung.java
+++ Weinbau/src/Empfehlung.java
@@ -2,7 +2,7 @@
 public class Empfehlung {
 	
 	boolean warnung;
-	int bewaessern; 
+	int bewaesserungsmenge; 
 	int duengen; 
 	int pflanzenschutz; 
 	String text; 
@@ -68,7 +68,7 @@ public class Empfehlung {
 	}
 	
 	public int sollBewaessern() {
-		return bewaessern; 
+		return bewaesserungsmenge; 
 	}
 	
 	public int sollPflanzenschutz() {
@@ -79,22 +79,55 @@ public class Empfehlung {
 		if(weinberg.getBodenfeuchtigkeit()>50) {
 			return;
 		}
+		
+		//berechnen wahrscheinlicher Niederschlag, Sonnenstunden und Temperatur über nächsten 7 Tage
 		double niederschlag = 0.0; 
 		double regenwahrscheinlichkeit = 0.0; 
-		for(int i = 0; i < 7 ; i++) {
+		double sonnenstunden = 0.0; 
+		double temperatur = 0.0;
+		for(int i = 10; i < 17 ; i++) { 	
 			Wetter wetter = Datenbank.getWetter(i, weinberg);
 			niederschlag = niederschlag + wetter.getNiederschlag();
 			regenwahrscheinlichkeit = regenwahrscheinlichkeit + wetter.getRegenwahrscheinlichkeit();
+			sonnenstunden = sonnenstunden + wetter.getSonnenstunden();
+			temperatur = temperatur + wetter.getTemp(); 
 		}
 		niederschlag = niederschlag / 7; 
 		regenwahrscheinlichkeit = regenwahrscheinlichkeit / 7 ; 
-//		if() {
-//			
-//		}else if() {
-//			
-//		}else {
-//			
-//		}
+		sonnenstunden = sonnenstunden / 7; 
+		temperatur = temperatur / 7; 
+		
+		if(regenwahrscheinlichkeit <50) { 	
+			niederschlag = 0.4 * niederschlag;
+		}else if(regenwahrscheinlichkeit >85) {
+			niederschlag = 0.8 * niederschlag; 
+		}else {
+			niederschlag = 0.7 * niederschlag; 
+		}
+		
+		//berechnen der benötigten Niederschlagsmenge in Abhängigkeit des erwarteten Niederschlags und der aktuellen Bodenfeuchtigkeit
+		// und in Abhängigkeit der benötigten Menge je nach Alter der Reben 
+		//if alter < 2 Jahre
+		bewaesserungsmenge = (int) ((( weinberg.getBodenfeuchtigkeit()/100)*10000) - niederschlag); 
+		//if alter > 2 Jahre 
+		bewaesserungsmenge = (int) (((weinberg.getBodenfeuchtigkeit()/100)*8000) - niederschlag); 
+		
+		//Temperatur und Sonnenstunden einberechnen 
+		if(temperatur <15) {
+			// zu vernachlässigende Verdunstung
+		}else if(temperatur <25) {
+			bewaesserungsmenge = (int) (bewaesserungsmenge * 1.1);
+		}else {
+			bewaesserungsmenge = (int) (bewaesserungsmenge * 1.3 );
+		}
+		
+		if(sonnenstunden <3) {
+			//vernachlässigbar 
+		}else if(sonnenstunden <8) {
+			bewaesserungsmenge = (int) (bewaesserungsmenge * 1.1 );
+		}else {
+			bewaesserungsmenge = (int) (bewaesserungsmenge * 1.3 );
+		}
 		
 		
 	}
