5699c3e00c2ebc365f8c875eec4452c1ccac82c8
D074003 <D074003@WDFN34367190A.emea.global.corp.sap>
2019-04-17 13:02:36 +0200

    Hinzufügen der Main Klasse (beinhaltet main Methode)

---
+++ Weinbau/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Datenbank initialisieren 
+		Datenbank.initialisiereWetter();
+		//GUI initialisieren
+	}
+
+}
