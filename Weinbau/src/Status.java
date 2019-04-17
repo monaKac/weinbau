b21ffea831a78fe62f3bcc847c1d01515c0f8476
D074003 <D074003@WDFN34367190A.emea.global.corp.sap>
2019-04-17 10:29:15 +0200

    Konstruktor und Setter/Getter zu Klasse Status hinzugefügt

--- Weinbau/src/Status.java
+++ Weinbau/src/Status.java
@@ -1,9 +1,43 @@
 
 public class Status {
-	double Prozent;
+	double prozent;
 	Weinbergstatus status; 
 	
 	public Status() {
-		
+		this.status = Weinbergstatus.WINTERRUHE; 
+		this.prozent = 0.0; 
 	}
+	
+	public Status(Weinbergstatus status) {
+		this.status = status; 
+		this.prozent = 0.0; 
+	}
+	
+	public Status(Weinbergstatus status, double prozent) {
+		this.status = status; 
+		this.prozent = prozent; 
+	}
+	
+	public void setProzent(double prozent) {
+		this.prozent = prozent; 
+	}
+	
+	public void setStatus(Weinbergstatus status, double prozent) {
+		this.status = status; 
+		this.prozent = prozent; 
+	}
+	
+	public void setStatus(Weinbergstatus status) {
+		this.status = status; 
+		this.prozent = 0.0; 
+	}
+	
+	public double getProzent() {
+		return prozent;
+	}
+	
+	public Weinbergstatus getWeinbergstatus() {
+		return status; 
+	}
+	
 }
