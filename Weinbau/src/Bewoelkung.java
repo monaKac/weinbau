adc0d960a010efd0a25eb17e6aa9cb4513887659
D074003 <D074003@WDFN34367190A.emea.global.corp.sap>
2019-04-30 16:59:12 +0200

    Icons Bewoelkung (enum) hinzugefügt

--- Weinbau/src/Bewoelkung.java
+++ Weinbau/src/Bewoelkung.java
@@ -1,32 +1,94 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public enum Bewoelkung {
 	SONNIG{
 		public String toString() {
 			return "Sonnig"; 
+		}
+		
+		public BufferedImage getIcon() {
+			BufferedImage img = null;
+			try {
+			    img = ImageIO.read(new File("Sonnig.jpg"));
+			} catch (IOException e) {
+			}
+			return img; 
 		}
 	}, 
 	BEWOELKT{
 		public String toString() {
 			return "Bewoelkt"; 
 		}
+		
+		public BufferedImage getIcon() {
+			BufferedImage img = null;
+			try {
+			    img = ImageIO.read(new File("Bewoelkt.jpg"));
+			} catch (IOException e) {
+			}
+			return img; 
+		}
 	}, 
 	REGEN{
 		public String toString() {
 			return "Regnerisch"; 
+		}
+		
+		public BufferedImage getIcon() {
+			BufferedImage img = null;
+			try {
+			    img = ImageIO.read(new File("Regen.jpg"));
+			} catch (IOException e) {
+			}
+			return img; 
 		}
 	}, 
 	SCHNEE{
 		public String toString() {
 			return "Schnee"; 
 		}
+		
+		public BufferedImage getIcon() {
+			BufferedImage img = null;
+			try {
+			    img = ImageIO.read(new File("Schnee.jpg"));
+			} catch (IOException e) {
+			}
+			return img; 
+		}
 	}, 
 	STURM{
 		public String toString() {
 			return "Sturm"; 
+		}
+		
+		public BufferedImage getIcon() {
+			BufferedImage img = null;
+			try {
+			    img = ImageIO.read(new File("Sturm.jpg"));
+			} catch (IOException e) {
+			}
+			return img; 
 		}
 	}, 
 	LEICHT_BEWOELKT{
 		public String toString() {
 			return "Leicht Bewoelkt"; 
 		}
+		
+		public BufferedImage getIcon() {
+			BufferedImage img = null;
+			try {
+			    img = ImageIO.read(new File("LeichtBewoelkt.jpg"));
+			} catch (IOException e) {
+			}
+			return img; 
+		}
 	};
+	
+
 }
