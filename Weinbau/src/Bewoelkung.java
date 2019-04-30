ba5ab08116441442e0131a8ecf4a83283395773b
D074003 <D074003@WDFN34367190A.emea.global.corp.sap>
2019-04-30 16:42:40 +0200

    ToString überschrieben von Bewoelkung

--- Weinbau/src/Bewoelkung.java
+++ Weinbau/src/Bewoelkung.java
@@ -1,3 +1,32 @@
 public enum Bewoelkung {
-	SONNIG, BEWOELKT, REGEN, SCHNEE, STURM, LEICHT_BEWOELKT;
+	SONNIG{
+		public String toString() {
+			return "Sonnig"; 
+		}
+	}, 
+	BEWOELKT{
+		public String toString() {
+			return "Bewoelkt"; 
+		}
+	}, 
+	REGEN{
+		public String toString() {
+			return "Regnerisch"; 
+		}
+	}, 
+	SCHNEE{
+		public String toString() {
+			return "Schnee"; 
+		}
+	}, 
+	STURM{
+		public String toString() {
+			return "Sturm"; 
+		}
+	}, 
+	LEICHT_BEWOELKT{
+		public String toString() {
+			return "Leicht Bewoelkt"; 
+		}
+	};
 }
