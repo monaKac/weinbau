ae08edf9a84da647026c00739b1247249f6e9344
D074003 <D074003@WDFN34367190A.emea.global.corp.sap>
2019-05-06 09:48:02 +0200

    Tostring() ueberschrieben

--- Weinbau/src/Weinbergstatus.java
+++ Weinbau/src/Weinbergstatus.java
@@ -1,4 +1,36 @@
 
 public enum Weinbergstatus {
-	WINTERRUHE, REBSCHNITT, REBERZIEHUNG, BODENARBEIT, PFLANZENSCHUTZ, BEFRUCHTUNG, LAUBARBEIT, ERNTE;
+	WINTERRUHE{
+		public String toString() {
+			return "Winterruhe"; 
+		}
+	}, REBSCHNITT{
+		public String toString() {
+			return "Rebschnitt"; 
+		}
+	}, REBERZIEHUNG{
+		public String toString() {
+			return "Reberziehung"; 
+		}
+	}, BODENARBEIT{
+		public String toString() {
+			return "Bodenarbeit"; 
+		}
+	}, PFLANZENSCHUTZ{
+		public String toString() {
+			return "Pflanzenschutz"; 
+		}
+	}, BEFRUCHTUNG{
+		public String toString() {
+			return "Befruchtung"; 
+		}
+	}, LAUBARBEIT{
+		public String toString() {
+			return "Laubarbeit"; 
+		}
+	}, ERNTE{
+		public String toString() {
+			return "Ernte"; 
+		}
+	};
 }
