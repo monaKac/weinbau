21748c729f7dd4d145c7b554a19f90719747ad8e
D074003 <D074003@WDFN34367190A.emea.global.corp.sap>
2019-05-08 16:01:38 +0200

    Schnittstelle angepasst

--- Weinbau/src/SchnittstelleFeldanwendung.java
+++ Weinbau/src/SchnittstelleFeldanwendung.java
@@ -1,23 +1,55 @@
-
+/**
+ * 
+ * @author D074003
+ * @version 1.0
+ * Ist die Schnittstelle der GUI zu dem Weinberg
+ * Fuehrt Aktionen wie Bewaeserung, Pflanzenschutz und Duengen aus. 
+ * In weiteren Versionen wird es fertiggestellt sein, bisher ist es noch ein Prototype.
+ */
 public class SchnittstelleFeldanwendung {
 
+	/**
+	 * 
+	 * @param weinberg
+	 * @param menge
+	 * Startet Bewaesserung eines vollautomatischen Weinberges
+	 */
 	public static void starteBewaesserung(Weinberg weinberg, int menge) {
-		System.out.println("Starte Bewässerung"); 
+		System.out.println("Starte Bewaesserung"); 
 	}
 	
+	/**
+	 * 
+	 * @param weinberg
+	 * @param menge
+	 * Startet Pflanzenschutz eines vollautomatischen Weinberges
+	 */
 	public static void startePflanzenschutz(Weinberg weinberg, int menge) {
-		
+		System.out.println("Starte Pflanzenschutz: "+menge+"ml");
 	}
 	
+	/**
+	 * 
+	 * @param weinberg
+	 * @param menge
+	 * Startet Duengen eienes vollautomatischen Weinberges
+	 */
 	public static void starteDuengen(Weinberg weinberg, int menge) {
-		
+		System.out.println("Starte Duengen: "+menge + "ml");
 	}
 	
+	/**
+	 * 
+	 * @param weinberg
+	 * @param status
+	 * Setzt den Status des Weinberges auf den neuen Status 
+	 */
 	public static void naechsterStatus(Weinberg weinberg, Weinbergstatus status) {
+		System.out.println("Uebergang zu naechstem Status "+ status.toString());
 		weinberg.setStatus(new Status(status));
 	}
 	
-
+	
 	
 	
 }
