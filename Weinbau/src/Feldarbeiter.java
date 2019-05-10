91a7dfc640fcb9bf380b4e4b199e38bde14f9fb8
D074003 <D074003@WDFN34367190A.emea.global.corp.sap>
2019-05-10 09:11:44 +0200

    Feldarbeiter Anwendung Oberflaeche erweitert

--- Weinbau/src/Feldarbeiter.java
+++ Weinbau/src/Feldarbeiter.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -93,6 +95,17 @@ public class Feldarbeiter extends JFrame {
 		    };
 		    comboWeinberg.addItemListener(itemListener);
 		
+		Timer timerKommentar = new Timer();
+		timerKommentar.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				if (inKommentar.isEditable()==false)
+				inKommentar.setText(selectedWeinberg.getKommentar());
+			}
+		}, 0, 1800);
+		    
 		this.setVisible(true);
+		
 	}
 }
